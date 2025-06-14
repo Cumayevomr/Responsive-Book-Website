20b8428868c623ebb12e18f623e2e0e344dea601
Cumayevomr <cumayevomr8@gmail.com>
2025-06-14 13:39:19 +0400

    log in and sign in click generating

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const searchButton = document.getElementById('search-button'),
       searchClose = document.getElementById('search-close'),
       searchContent = document.getElementById('search-content')
 
-/* Menu show */
+/* Search show */
 
 if(searchButton){
     searchButton.addEventListener('click', () =>{
@@ -11,18 +11,34 @@ if(searchButton){
     })
 }
 
-/* Menu hidden */
+/* Search hidden */
 
 if(searchClose){
     searchClose.addEventListener('click', () =>{
         searchContent.classList.remove('show-search')
     })
 }
-      
-
 
 /*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close'),
+      loginContent = document.getElementById('login-content')
 
+/* login show */
+
+if(loginButton){
+    loginButton.addEventListener('click', () =>{
+        loginContent.classList.add('show-login')
+    })
+}
+
+/* login hidden */
+
+if(loginClose){
+    loginClose.addEventListener('click', () =>{
+        loginContent.classList.remove('show-login')
+    })
+}
 
 /*=============== ADD SHADOW HEADER ===============*/
 
